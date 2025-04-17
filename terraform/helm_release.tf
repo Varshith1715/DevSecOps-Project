28e35fc28617ddd282969b8bc48932357dc79b7a
Varshith1715 <pasupunurivarshith@gmail.com>
2025-04-17 22:28:41 +0530

    Update helm_release.tf

--- terraform/helm_release.tf
+++ terraform/helm_release.tf
@@ -1,8 +1,6 @@
 resource "helm_release" "taxi_booking" {
   name      = "taxi-booking"
   namespace = "default"
-  chart     = "${path.module}/terraform/web-app-chart"
-  values = [
-    file("${path.module}/terraform/web-app-chart/values.yaml")
-  ]
+  chart     = "${path.module}/web-app-chart"
+  values    = [ file("${path.module}/web-app-chart/values.yaml") ]
 }
