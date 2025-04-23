2f4752a9e2ea843ba1177081e2a3439c5645cda6
Varshith1715 <pasupunurivarshith@gmail.com>
2025-04-23 20:16:27 +0530

    Update main.tf

--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -4,17 +4,19 @@ resource "aws_security_group" "jenkins_sg" {
   vpc_id      = var.vpc_id
 
   ingress {
+    # Allow SSH access (for management)
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
+    # Allow HTTP access to Jenkins dashboard (default port 8080)
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 
   ingress {
@@ -26,6 +28,7 @@ resource "aws_security_group" "jenkins_sg" {
 
 
   egress {
+    # Allow all outbound traffic (you may limit it as needed)
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
