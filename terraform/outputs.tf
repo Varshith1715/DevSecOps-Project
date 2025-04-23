fc1b1847e601f13cf88eed4e88affba480f61fd7
Varshith1715 <pasupunurivarshith@gmail.com>
2025-04-23 20:11:49 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,39 @@
-output "vpc_id" {
-  description = "VPC ID"
-  value       = module.vpc.vpc_id
+# VPC Module
+module "vpc" {
+  source              = "./modules/vpc/"
+  availability_zones  = var.availability_zones
+  name_prefix         = var.name_prefix
+  vpc_cidr            = var.vpc_cidr
+  public_subnet_cidrs = var.public_subnet_cidrs
 }
 
-# output "eks_cluster_name" {
-#   description = "EKS Cluster Name"
-#   value       = module.eks.cluster_name
-# }
-
-output "jenkins_instance_id" {
-  description = "Jenkins EC2 instance ID"
-  value       = module.jenkins.jenkins_instance_id
+# EKS Module
+module "eks" {
+  source            = "./modules/eks/"
+  aws_region        = var.aws_region
+  vpc_id            = module.vpc.vpc_id
+  public_subnet_ids = module.vpc.public_subnet_ids
+  name_prefix       = var.name_prefix
+  desired_size      = var.eks_cluster_desired_size
+  max_size          = var.eks_cluster_max_size
+  min_size          = var.eks_cluster_min_size
+  instance_type     = var.instance_type
 }
+
+# Jenkins Module
+module "jenkins" {
+  source            = "./modules/jenkins/"
+  key_name          = var.key_name
+  ami_id            = var.ami_id
+  subnet_id         = module.vpc.public_subnet_ids[0]
+  security_group_id = module.vpc.jenkins_sg_id
+
+  aws_region            = var.aws_region
+  vpc_id                = module.vpc.vpc_id
+  subnet_ids            = module.vpc.public_subnet_ids
+  eks_cluster_name      = module.eks.cluster_name
+  jenkins_instance_type = var.jenkins_instance_type
+}
+
+
 
