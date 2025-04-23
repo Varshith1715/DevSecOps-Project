ab2f9cadec0dbae08283aeb54686395544c10e06
Varshith1715 <pasupunurivarshith@gmail.com>
2025-04-23 20:03:08 +0530

    Update main.tf

--- terraform/modules/jenkins/main.tf
+++ terraform/modules/jenkins/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "jenkins_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs (be cautious with this, use specific IPs in production)
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   ingress {
@@ -16,14 +16,14 @@ resource "aws_security_group" "jenkins_sg" {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs (be cautious with this, use specific IPs in production)
+    cidr_blocks = ["0.0.0.0/0"]  
   }
 
   ingress {
   from_port   = 8090
   to_port     = 8090
   protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]  # Make restrictive if possible
+  cidr_blocks = ["0.0.0.0/0"]  
 }
 
 
